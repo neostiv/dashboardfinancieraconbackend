5d6b58480d87b6b673847d33073aafa10e784f54
John Torres <neostiv@gmail.com>
2023-05-15 11:23:31 -0500

    Let's gitignore

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,10 +1,13 @@
 //eslint-disable-next-line
 import React from 'react';
-import { Box, useTheme } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import Row3 from './Row3';
+import Row2 from './row2';
+import Row1 from './row1';
 
-type Props = {};
 
-const gridTemplate =    `
+
+const gridTemplateLargeScreens =    `
   " a b c "
   " a b c "
   " a b c "
@@ -15,30 +18,64 @@ const gridTemplate =    `
   " g h i "
   " g h j "
   " g h j "
-`
+`;
 
-const Dashboard = (props: Props) => {
+const gridTemplateSmallScreens = `
+" a "
+" a "
+" a "
+" a "
+" a "
+" b "
+" b "
+" b "
+" b "
+" c "
+" c "
+" c "
+" d "
+" d "
+" d "
+" e "
+" e "
+" f "
+" f "
+" f "
+" g "
+" g "
+" g "
+" h "
+" h "
+" h "
+" h "
+" i "
+" i "
+" j "
+" j "
+`
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
-    const { palette } = useTheme();
+    
 
   return <Box width = "100%" height="100%" display="grid" gap="1.5rem"
-  sx={{
+  sx={
+    isAboveMediumScreens ? {
     gridColumn: "repeat(3, minmax(370px, 1fr))",
     gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-    gridTemplateAreas: gridTemplate,
+    gridTemplateAreas: gridTemplateLargeScreens,
+  } 
+  :{
+    gridAutoColumn: "1fr",
+    gridAutoRows: "80px",
+    gridTemplateAreas: gridTemplateSmallScreens,
+  }
 
-  }}
+}
   >
-    <Box bgcolor="#fff" gridArea="a"></Box>
-    <Box bgcolor="#fff" gridArea="b"></Box>
-    <Box bgcolor="#fff" gridArea="c"></Box>
-    <Box bgcolor="#fff" gridArea="d"></Box>
-    <Box bgcolor="#fff" gridArea="e"></Box>
-    <Box bgcolor="#fff" gridArea="f"></Box>
-    <Box bgcolor="#fff" gridArea="g"></Box>
-    <Box bgcolor="#fff" gridArea="h"></Box>
-    <Box bgcolor="#fff" gridArea="i"></Box>
-    <Box bgcolor="#fff" gridArea="j"></Box>
+      
+    <Row1 />
+    <Row2 />
+    <Row3 />
 
 
 </Box>;
